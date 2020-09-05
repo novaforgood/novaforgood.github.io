68a4e35f91d24d653bd254d957923b785250a097
Max Wu <chengmaxwu@gmail.com>
2020-09-05 02:10:58 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# novaforgood.github.io
---
+++ component---src-pages-404-js-bb38605d461230fd60a0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(n,t,c){"use strict";c.r(t);var e=c("q1tI"),u=c("Wbzz");t.default=function(){return Object(e.useEffect)((function(){Object(u.c)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bb38605d461230fd60a0.js.map
